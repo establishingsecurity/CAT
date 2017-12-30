e48cf7c221af67d97756f8838a1a1b45ddda7cd3
malet <malet@invalid>
2017-12-30 13:15:52 +0100

    Bootstrapping project

---
+++ setup.py
@@ -0,0 +1,52 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('cat/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = []
+
+setup(
+    name='cat',
+    version=version,
+    description='',
+    long_description=readme,
+    author='random-access',
+    author_email='random-access@invalid',
+    maintainer='random-access',
+    maintainer_email='random-access@invalid',
+    url='https://github.com/random-access/cat',
+    license='BSD',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
