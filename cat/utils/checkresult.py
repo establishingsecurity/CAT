2930698e97098678add38b2fdedecf97736300ba
random-access <random-access@invalid>
2017-12-30 17:11:42 +0100

    Fixed rsa test cases

--- cat/utils/checkresult.py
+++ cat/utils/checkresult.py
@@ -1,14 +1,12 @@
 from enum import Enum
 
-class CheckResult():
-
-    def __init__(self, severity = Severity.OK, comment = ''):
-        self.severity = severity
-        self.comment = comment
-
 class Severity(Enum):
     OK = 0
     SUSPICIOUS = 1
     CRITICAL = 2
 
+class CheckResult():
 
+    def __init__(self, severity = Severity.OK, comment = ''):
+        self.severity = severity
+        self.comment = comment
