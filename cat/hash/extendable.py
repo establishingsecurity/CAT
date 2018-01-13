b51a7e8a4581ee32becae27d8bbf0b626e9afab0
malet <malet@invalid>
2018-01-13 07:25:25 +0100

    Something is wrong, but I do not know what

--- cat/hash/extendable.py
+++ cat/hash/extendable.py
@@ -1,16 +1,15 @@
 from Cryptodome.Hash import SHA256
-from ctypes import c_ubyte
+from Cryptodome.Util.number import bytes_to_long
+from ctypes import c_ulonglong
 
 def load(load_state: bytes, input_length: int):
     r"""
-    This function takes the digest of a Merkle–Damgård construction, the length
-    of the original output and a message to extend it with, and returns the a
-    copy of the original hash function
+    This function takes the digest of a Merkle–Damgård construction, the input
+    length previously given to the hashfunction and returns the a copy of the
+    original hash function
 
-    >>> h1 = SHA256.new(b'deadbeef')
-    >>> h1.digest()
-    b'+\xaf\x1f@\x10]\x95\x01\xfe1\x9a\x8e\xc4c\xfd\xf42Z*]\xf4E\xad\xf3\xf5r\xf6&%6x\xc9'
-    >>> h2 = load(h1.digest(), len(b'deadbeef'))
+    >>> h1 = SHA256.new(b'A'*64)
+    >>> h2 = load(h1.digest(), 64)
     >>> h1.update(b'deadbeef')
     >>> h2.update(b'deadbeef')
     >>> h1.digest() == h2.digest()
@@ -18,18 +17,18 @@ def load(load_state: bytes, input_length: int):
     """
 
     # Prepare the load_state
-    # TODO: Do padding and stuff
+    ls = [c_ulonglong(int.from_bytes(load_state[i:i+8], 'big')) for i in range(0, 64, 8)]
 
     # Creating the new hash object
-    hash_object = SHA256.new()
+    hash_object = SHA256.new(b'\x00' * input_length)
 
     # Retrieving the state as byte array
     state_pointer = hash_object._state.get()
-    state_type = c_ubyte * 64
-    state = state_type.from_address(state_pointer.value)
+    state_type = c_ulonglong * 8
+    state = state_type.from_buffer(state_type.from_address(state_pointer.value))
 
     # Changing the state
-    for (i, b) in enumerate(state):
+    for (i, b) in enumerate(ls):
         state[i] = b
 
     return hash_object
