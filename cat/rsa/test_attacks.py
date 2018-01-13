d0e2e5fa6746d81bffd748ebcb156d43986ecfa6
malet <malet@invalid>
2018-01-13 18:39:31 +0100

    Adding hypothesis and slow running test invokation with ./test.sh -s

--- cat/rsa/test_attacks.py
+++ cat/rsa/test_attacks.py
@@ -4,16 +4,40 @@ from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_v1_5
 from gmpy2 import powmod
 
+from .attacks import common_divisor, LSBOracle
 
-from .attacks import LSBOracle
+import pytest
+from hypothesis import given, reject, assume, settings
+from hypothesis.strategies import integers, text, sampled_from
 
-def test_lsb_oracle():
-    key = RSA.generate(1024)
+@pytest.fixture()
+def key():
+    return RSA.generate(1024)
+
+
+@given(integers(), integers())
+@settings(deadline=None)
+@pytest.mark.slow
+def test_common_divisor(key, x, plain):
+    assume(0 < x < key.n)
+    assume(0 < plain < key.n)
+    sk = common_divisor(key.publickey(), key.p * x)
+    cipher = powmod(plain, key.e, key.n)
+    assert int(powmod(cipher, sk.d, sk.n)) == plain
+
+
+@given(integers())
+@settings(deadline=None)
+@pytest.mark.slow
+def test_lsb_oracle(key, plain):
+    assume(0 < plain < key.n)
+
     class TestLSBOracle(LSBOracle):
         def query(self, m: int):
             return powmod(m, key.d, self.pk.n) % 2
 
-    target = powmod(255, key.e, key.n)
+    target = powmod(plain, key.e, key.n)
     o = TestLSBOracle(key.publickey(), target)
-    assert o.run() == 255
+    assert o.run() == plain
+
 
--- test.sh
+++ test.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+RUN_ARGS=""
+while [[ $# -gt 0 ]]
+	do
+	key="$1"
+	case $key in
+		-s|--slow)
+		RUN_ARGS="$RUN_ARGS --slow -n auto"
+		shift # past argument
+		;;
+		*)    # unknown option
+		shift # past argument
+		;;
+	esac
+done
+
 docker build -t cat-test -f Dockerfile.test .
-docker run --rm cat-test
+docker run --rm --env RUN_ARGS="$RUN_ARGS" cat-test
 docker rmi cat-test
