9f41f61e8102be51a30b5f6d769bf1ebf3c3e425
malet <malet@invalid>
2018-01-13 18:39:05 +0100

    Adding parallel tests to setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
 
     install_requires=REQUIRES,
-    tests_require=['coverage', 'pytest'],
+    tests_require=['coverage', 'pytest', 'pytest-xdist'],
 
     packages=find_packages(),
 )
