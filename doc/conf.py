f80d98b86e14107f13f9ac42d949899682eefc1f
crave <rleanderschroeder@gmail.com>
2018-10-30 14:22:24 +0100

    Add gmpy2 mock

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
 sys.path.insert(0, os.path.abspath('../cat/'))
 
 
@@ -182,3 +183,11 @@ epub_exclude_files = ['search.html']
 source_parsers = {
    '.md': 'recommonmark.parser.CommonMarkParser',
 }
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['gmpy2']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
