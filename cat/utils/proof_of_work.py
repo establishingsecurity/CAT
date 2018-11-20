182dc4a722f8339a7950f40ccdc7d8d7f625906f
Michael Strasser <mst1409@gmx.at>
2018-11-20 13:57:49 +0100

    Add documentation for the pattern-based proof of work utility function

--- cat/utils/proof_of_work.py
+++ cat/utils/proof_of_work.py
@@ -104,6 +104,60 @@ def compute_prefix(hash_function, suffix, condition, input_source, **kwargs):
 
 
 def compute_with_pattern(hash_function, pattern, input_source, **kwargs):
+    """
+    Compute a Proof of Work based on the given pattern.
+
+    >>> # Compute a digest that starts and ends with a '1'
+    >>> def input_source(init):
+    ...     for bf in generate_brute_force(init):
+    ...         yield bytes(bf)
+    >>> pow_ = compute_with_pattern(Cryptodome.Hash.SHA1, r'^1{h}1$', input_source([0]))
+    >>> print(bytes(pow_).hex())
+    0d
+    >>> print(Cryptodome.Hash.SHA1.new(data=bytes(pow_)).hexdigest())
+    11f4de6b8b45cf8051b1d17fa4cde9ad935cea41
+
+    >>> # Compute a digest with '123' somewhere
+    >>> pow_ = compute_with_pattern(Cryptodome.Hash.SHA1, r'{h}123{h}', input_source([0]))
+    >>> print(bytes(pow_).hex())
+    0178
+    >>> print(Cryptodome.Hash.SHA1.new(data=bytes(pow_)).hexdigest())
+    754da6b34a4b1c42f5aa7ef08123ca5b5ac72f03
+
+    >>> # Compute a digest with '1', '2' and '3' separated by at least one
+    >>> # value in-between
+    >>> pow_ = compute_with_pattern(Cryptodome.Hash.SHA1, r'{h}1{h}2{h}3{h}', input_source([0]))
+    >>> print(bytes(pow_).hex())
+    00
+    >>> print(Cryptodome.Hash.SHA1.new(data=bytes(pow_)).hexdigest())
+    5ba93c9db0cff93f52b521d7420e43f6eda2784f
+
+    :param pattern:       The regex pattern that the resulting digest should conform to.
+                          You can insert '{h}' wherever you want non fixed values, or '{h_}'
+                          for an optional non fixed value (i.e. '{h_}' can result in '', '{h}'
+                          reliably results in some character in the digest), which is just
+                          r'\w+' and r'\w*' for people who hate regex.
+                          The rest of the string is interpreted as a regex pattern. You want
+                          to use the `r` prefix for Python to interpret the string as 'raw',
+                          allowing you to avoid the backslash plague (e.g. you can pass `\d`
+                          as `r'\d'` instead of `'\\d'`).
+    :type pattern:        :class:`str`
+    :param hash_function: The hash function to use (e.g. :class:`Crypto.Hash.SHA256.SHA256Hash`).
+    :type hash_function:  All modern and history hash algorithms listed in :doc:`src/hash/hash`.
+    :param input_source:  A generator that creates input values for the hash function. Defaults
+                          to brute force starting at `0`.
+    :type input_source:   :class:`generator`
+    :param action:        An action performed on the `base_input` and the values from the
+                          `input_source`. Defaults to appending the values to `base_input`.
+    :type action:         :class:`function`
+    :param kwargs:        Additional keyword arguments that get passed to `hash_function.new`
+                          (e.g. `digest_bytes` for :class:`Crypto.Hash.BLAKE2s`).
+    :type kwargs:         :class:`dict`
+    :returns:             A bytestring that produces a `hash_function` hexdigest of the form
+                          specified by `pattern` when used as input for `hash_function.new`.
+                          `suffix`.
+    :rtype:               :class:`bytes`
+    """
     block_size = getattr(hash_function, 'block_size', None)
     if block_size and len(pattern.replace('{h}', '').replace('{h_}', '')) >= block_size:
         raise Exception('Your pattern must be smaller than the block size of ' +
