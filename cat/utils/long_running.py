7c0639292efd4384a048e75e6758b7d07f342c59
malet <malet@invalid>
2018-11-22 13:53:07 +0100

    Better distinction between function calls

--- cat/utils/long_running.py
+++ cat/utils/long_running.py
@@ -1,19 +1,33 @@
 from functools import wraps
 from pickle import dumps, load
+from hashlib import sha256
 
 from cat.config import snapshots_path
 
-class Snapshot():
-    def __init__(self, args, kwargs, value):
-        self.value = value
+class SnapshotHeader():
+    def __init__(self, name, args, kwargs):
+        self.name = name
         self.args = args
         self.kwargs = kwargs
+
+    def __str__(self):
+        h = sha256()
+        h.update(dumps(self))
+        return h.hexdigest()
+
+
+class Snapshot():
+    def __init__(self, header, value):
+        self.header = header
+        self.value = value
+
 
 def long_running(fun):
     @wraps(fun)
     def with_state(*args, **kwargs):
         # TODO: distinguish snaps with arguments
-        snap_file = snapshots_path/fun.__name__
+        snap_header = SnapshotHeader(fun.__name__, args, kwargs)
+        snap_file = snapshots_path/str(snap_header)
 
         # Check if we cached the result
         if snap_file.is_file():
@@ -24,8 +38,9 @@ def long_running(fun):
 
         # Really run the function
         value = fun(*args, **kwargs)
-        snap = dumps(Snapshot(args, kwargs, value))
+        snap = dumps(Snapshot(snap_header, value))
         # Cache the result
+        snapshots_path.mkdir(exist_ok=True, parents=True)
         with snap_file.open('wb') as f:
             f.write(snap)
         return value
