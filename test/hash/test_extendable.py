a5fc4b60906b9185f7761a42067db5bd378e9344
ambiso <ambiso@invalid>
2018-11-22 22:15:59 +0100

    Use hashpumpy

---
+++ test/hash/test_extendable.py
@@ -0,0 +1,40 @@
+from Cryptodome.Hash import SHA256
+from Cryptodome.Util.number import bytes_to_long
+from ctypes import c_ulonglong
+from binascii import hexlify
+import struct
+
+from cat.hash.extendable import *
+
+import pytest
+from hypothesis import given, assume
+from hypothesis.strategies import binary, lists, composite
+
+
+@composite
+def byte_array(draw):
+    data = draw(lists(binary()))
+    return b''.join(data)
+
+
+@given(byte_array(), byte_array(), byte_array())
+@pytest.mark.slow
+def test_length_extension_attack_sha256(secret, original_data, data_to_add):
+    assume(len(original_data) > 0)
+    assume(len(data_to_add) > 0)
+    h = SHA256.new(secret + original_data).hexdigest()
+    digest, message = hashpump(h, original_data, data_to_add, len(secret))
+    assert message.startswith(original_data)
+    assert message != original_data
+    assert message.endswith(data_to_add)
+    pad = message[len(original_data):-len(data_to_add)]
+    assert original_data + pad + data_to_add == message
+    print(f'sha256="{h}"')
+    print(f'secret={secret}')
+    print(f'original_data={original_data}')
+    print(f'data_to_add={data_to_add}')
+    print(f'digest="{digest}"')
+    print(f'message={message}')
+    print(f'pad={pad}')
+    print()
+    assert SHA256.new(secret + message).hexdigest() == digest
