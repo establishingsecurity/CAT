e679d4be0299294691beab89f540d288c6dd1ceb
ambiso <ambiso@invalid>
2018-11-23 00:09:36 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     extras_require={
         'dev': ['ipython'],
         'test': ['pytest', 'hypothesis',  'tox', 'pytest-benchmark'],
-        'doc': ['sphinx', 'recommonmark', 'sphinx_rtd_theme']
+        'doc': ['sphinx', 'sphinx_rtd_theme', 'sphinxcontrib-apidoc']
     }
 )
