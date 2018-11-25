d6aa69cc5ff8d332cd4d06ae6eb7f4d2977bb951
ambiso <ambiso@invalid>
2018-11-25 02:47:41 +0100

    Update conf

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 import subprocess
 import distutils.core
+import guzzle_sphinx_theme
 
 
 sys.path.insert(0, os.path.abspath('../../cat/'))
@@ -48,6 +49,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinxcontrib.apidoc',
+    'guzzle_sphinx_theme',
 ]
 
 # Links to other documentations
@@ -86,13 +88,17 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Crypto Attack Toolkit",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
