4a7056ec5e70c300dd0d2948d4d8fa67409f52d4
Michael Strasser <mst1409@gmx.at>
2018-11-26 15:52:16 +0100

    Add tests for `pmap` wrapper function

--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -1,8 +1,45 @@
-from cat.utils.utils import generate_brute_force
+from Cryptodome.Hash import SHA1
 
+
+from cat.utils.utils import generate_brute_force, pmap
+
+
+class TestPMap:
+    """Tests for the parallel map function."""
+
+    def test_doc_string(self):
+        """Test for the example in the docstring codeblock."""
+        fun = lambda d: SHA1.new(d).hexdigest()
+
+        inputs = [b'1234', b'5678', b'9101', b'1121']
+        outputs = ['7110eda4d09e062aa5e4a390b0a572ac0d2c0220',
+                   '2abd55e001c524cb2cf6300a89ca6366848a77d5',
+                   'f5a6fe40024c28967a354e591bb9fa21b784bf00',
+                   '784e9240155834852dff458a730cceb50229df32']
+
+        assert (pmap(fun, inputs) == outputs)
+
+        filter_ = lambda d: d.endswith('0')
+        outputs = ['7110eda4d09e062aa5e4a390b0a572ac0d2c0220',
+                   'f5a6fe40024c28967a354e591bb9fa21b784bf00']
+        assert (pmap(fun, inputs, filter_=filter_) == outputs)
+
+    def test_multiple_inputs(self):
+        """Test whether map behaves correctly on multiple inputs."""
+        fun = lambda x,y: x + y
+        xs = [1, 2, 3, 4, 5]
+        ys = [1, 2, 3, 4, 5]
+
+        zs = [2, 4, 6, 8, 10]
+        assert (pmap(fun, xs, ys) == zs)
+
+        filter_ = lambda z: z > 5
+        zs = [6, 8, 10]
+        assert (pmap(fun, xs, ys, filter_=filter_) == zs)
 
 class TestBruteForce:
     """Tests for the brute force generator."""
+
     def test_first_yield(self):
         """Test whether first value is start value."""
         bf = generate_brute_force([0])
