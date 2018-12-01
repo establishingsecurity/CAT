e0ef2aa6031f34e5e789cbc3a23a0e0d6e84eb26
malet <malet@invalid>
2018-12-01 20:29:16 +0100

    FINALLY

--- cat/symmetric/attacks.py
+++ cat/symmetric/attacks.py
@@ -3,28 +3,42 @@ from bitstring import BitArray
 
 def cbc_padding_oracle(iv, target, oracle):
     """
-    The CBC Padding Oracle attack decrypts a target ciphertext with the help of an oracle that reports back whether the plaintext is padded correctly or not.
+    The CBC Padding Oracle attack decrypts a target ciphertext with the help of
+    an oracle that reports back whether the plaintext is padded correctly or
+    not.
     """
+    # TODO: This only works for full padding blocks
+    plaintext = []
     block_length = len(iv)
-    for block_count, (iv_block, target_block) in enumerate(
-        zip([iv] + ciphertext[:-1], ciphertext)
-    ):
-        plaintext_block = b""
-        if block_count == 0:
-            plaintext_length = cbc_padding_oracle_length(iv, target, oracle)
+    ciphertext = [iv] + target
 
-        for i in reversed(range(1, block_length + 1)):
-            padder = BitArray(bytes=(i.to_bytes(1) * i), length=block_length)
+    # Get the padding first
+    plaintext_length = cbc_padding_oracle_length(iv, target, oracle)
+    padding_block = BitArray(
+        bytes=plaintext_length.to_bytes(1, byteorder="big") * plaintext_length
+    )
+
+    for iv_block, target_block in zip(ciphertext[:-2], ciphertext[1:-1]):
+        plaintext_block = BitArray(int=0x0, length=block_length * 8)
+        for i in range(1, block_length + 1):
+            pos_bit = (i-1) * 8
             for guess in range(0, 256):
-                iv_guess = (
-                    BitArray(bytes=iv_block)
-                    ^ (BitArray(int=guess, length=block_length) << i)
-                    ^ padder
+                pad_bytes = BitArray(
+                    bytes=(
+                        b"\x00" * (block_length - i)
+                        + i.to_bytes(1, byteorder="big") * i
+                    )
                 )
-                if oracle(iv_guess, target_block):
-                    plaintext_block += guess
-        plaintext_block = reversed(plaintext_block)
-        yield plaintext_block
+                iv_bytes = BitArray(bytes=iv_block)
+                guess_bytes = BitArray(int=guess, length=block_length * 8) << pos_bit
+                iv_guess = iv_bytes ^ guess_bytes ^ pad_bytes ^ plaintext_block
+                # import ipdb; ipdb.set_trace()
+                if oracle(iv_guess.tobytes(), target_block):
+                    plaintext_block.overwrite(BitArray(int=guess, length=8), (block_length*8) - (i*8))
+                    break
+
+        plaintext.append(plaintext_block.tobytes()[::-1])
+    return plaintext
 
 
 def cbc_padding_oracle_length(iv, target, oracle):
@@ -33,7 +47,11 @@ def cbc_padding_oracle_length(iv, target, oracle):
     It returns the actual length of the plaintext
     """
     ciphertext_length = len(target) * len(iv)
-    iv_bytes = BitArray(bytes=target[-2], length=len(iv) * 8)
+    if len(target) >= 2:
+        iv_bytes = BitArray(bytes=target[-2], length=len(iv) * 8)
+    else:
+        iv_bytes = BitArray(bytes=iv, length=len(iv) * 8)
+
     guess = BitArray(int=0x01, length=len(iv) * 8)
 
     # Slide the error term to the left byte by byte
--- test/symmetric/test_attacks.py
+++ test/symmetric/test_attacks.py
@@ -7,23 +7,30 @@ from cat.symmetric.util import block_chunks
 
 
 def test_cbc_padding_oracle_single():
-    plaintext = b"deadbee" * 16
+    plaintext = b"\x01" * 16
     key = unhexlify("deadbeef" * 4)
     base_iv = unhexlify("beefdead" * 4)
     cipher = AES.new(key, AES.MODE_CBC, iv=base_iv)
 
-    ciphertext = list(block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size))
+    ciphertext = list(
+        block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size)
+    )
 
     def oracle(iv, ciphertext):
         cipher = AES.new(key, AES.MODE_CBC, iv=iv)
         plaintext = cipher.decrypt(ciphertext)
         try:
-            unpad(plaintext)
+            unpad(plaintext, AES.block_size)
             return True
         except:
             return False
 
-    assert plaintext == cbc_padding_oracle(base_iv, ciphertext, oracle)
+    recovered = b"".join(list(cbc_padding_oracle(base_iv, ciphertext, oracle)))
+    print()
+    print(plaintext)
+    print(recovered)
+    assert plaintext == recovered
+
 
 def test_cbc_padding_oracle_length():
     plaintext = b"deadbeef" * 7
@@ -31,7 +38,9 @@ def test_cbc_padding_oracle_length():
     base_iv = unhexlify("beefdead" * 4)
     cipher = AES.new(key, AES.MODE_CBC, iv=base_iv)
 
-    ciphertext = list(block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size))
+    ciphertext = list(
+        block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size)
+    )
 
     def oracle(iv, ciphertext):
         cipher = AES.new(key, AES.MODE_CBC, iv=iv)
@@ -44,13 +53,16 @@ def test_cbc_padding_oracle_length():
 
     assert len(plaintext) == cbc_padding_oracle_length(base_iv, ciphertext, oracle)
 
+
 def test_cbc_padding_oracle_length_full():
     plaintext = b"deadbeef" * 8
     key = unhexlify("deadbeef" * 4)
     base_iv = unhexlify("beefdead" * 4)
     cipher = AES.new(key, AES.MODE_CBC, iv=base_iv)
 
-    ciphertext = list(block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size))
+    ciphertext = list(
+        block_chunks(cipher.encrypt(pad(plaintext, AES.block_size)), AES.block_size)
+    )
 
     def oracle(iv, ciphertext):
         cipher = AES.new(key, AES.MODE_CBC, iv=iv)
