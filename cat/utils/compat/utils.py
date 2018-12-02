afd1a5262eac9f171bd721f702bdbe2b3fb6354d
ambiso <ambiso@invalid>
2018-12-02 15:39:13 +0100

    Update hex_str

--- cat/utils/compat/utils.py
+++ cat/utils/compat/utils.py
@@ -1,9 +1,5 @@
 import sys
 from binascii import hexlify
 
-if sys.version_info.major >= 3 and sys.version_info.minor >= 5:
-    def hex_str(byte_str):
-        return str(hexlify(byte_str), 'utf-8')
-else:
-    def hex_str(byte_str):
-        return hexlify(byte_str)
+def hex_str(byte_str):
+    return hexlify(byte_str).decode('utf-8')
