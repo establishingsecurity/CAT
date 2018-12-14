91855511f8adac8409a5685a8096734ee032fa4d
malet <malet@invalid>
2018-12-14 21:19:04 +0100

    Commenting stuff

--- cat/prng/lcg.py
+++ cat/prng/lcg.py
@@ -1,4 +1,5 @@
-from fpylll import IntegerMatrix, LLL
+from fpylll import LLL, IntegerMatrix
+
 from sympy import Matrix
 
 
@@ -17,15 +18,25 @@ def fplll_to_matrix(matrix):
 
 def reconstruct_lower_bits(L, m, ys):
     """
-    Reconstructs the lower bits of a system of linear congurential equations
+    Reconstructs the lower bits of a system of linear congurential equations in lattice form with
+    L * xs = 0 mod m for solution xs.
+    It works by computing a smaller basis B from the basis L and solving equations in the smaller basis
+
     :param L: The system of linear equation in matrix form (evaluating to 0)
     :param m: The modulus used for all the equations
-    :param ys: The higher bits that are known for the variables
-    :return: The lower bits for the variables
+    :param ys: Partial solutions for the variables (xs = ys + zs)
+    :return: The remaining operands of the solutions zs
     """
+    # Reducing the lattice basis L to a smaller basis B
     B = fplll_to_matrix(LLL.reduction(matrix_to_fplll(L)))
+
     Bys = Matrix(B @ ys)
+
+    # TODO: There might be a better solution to find the individual ks
+    # NB: B * (ys + zs) = m * ks for some ks
     ks = Matrix([round(x) for x in Bys / m])
+
+    # We know solve the system of linear equations B zs = m * ks - B ys for zs
     Bzs = Matrix(m * ks - Bys)
     zs = B.solve(Bzs)
     assert B @ zs == Bzs
