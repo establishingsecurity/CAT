f683adccdce439dee8058bd0e62dc08e7a9e7dc5
ambiso <ambiso@invalid>
2018-12-14 21:03:23 +0100

    Remove testcase and debug prints

--- cat/prng/lcg.py
+++ cat/prng/lcg.py
@@ -5,7 +5,6 @@ from sympy import Matrix
 
 def matrix_to_fplll(matrix):
     assert len(matrix.shape) == 2
-    print(matrix.shape)
     rv = IntegerMatrix(*matrix.shape)
     for i, elem in enumerate(matrix):
         rv[i // matrix.shape[0], i % matrix.shape[1]] = int(elem)  # TODO CHECK
@@ -49,16 +48,3 @@ def get_upper_bits(v, n=None):
     return [x - x % 2 ** n for x in v]
 
 
-def test_reconstruct_lower_bits():
-    m = 4294967291
-    a = 598176085
-    s = 252291025
-    L = Matrix([[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2, 0, -1, 0], [a ** 3, 0, 0, -1]])
-    xs = Matrix([1477951715, 3597964208, 2802631510, 3169049466])
-    ys = Matrix(get_upper_bits(xs, 16))
-    assert ys == Matrix([1477902336, 3597926400, 2802581504, 3168993280])
-    zs = reconstruct_lower_bits(L, m, ys)
-    expected_zs = Matrix(xs - ys)
-    assert expected_zs == Matrix([49379, 37808, 50006, 56186])
-    assert zs == expected_zs
-    assert xs == (ys + zs)
