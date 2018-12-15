14e8e089c8aff4c70882de0bbb2e8daee7f361c5
ambiso <ambiso@invalid>
2018-12-15 14:52:22 +0100

    Remove sympy implementation

--- cat/prng/lcg.py
+++ cat/prng/lcg.py
@@ -1,18 +1,4 @@
-from fpylll import LLL, IntegerMatrix
-
-from sympy import Matrix
-
-
-def matrix_to_fplll(matrix):
-    assert len(matrix.shape) == 2
-    rv = IntegerMatrix(*matrix.shape)
-    for i, elem in enumerate(matrix):
-        rv[i // matrix.shape[0], i % matrix.shape[1]] = int(elem)  # TODO CHECK
-    return rv
-
-
-def fplll_to_matrix(matrix):
-    return Matrix([[*row] for row in matrix])
+from flint import fmpz_mat
 
 
 def reconstruct_lower_bits(L, m, ys):
@@ -26,23 +12,6 @@ def reconstruct_lower_bits(L, m, ys):
     :param ys: Partial solutions for the variables (xs = ys + zs)
     :return: The remaining operands of the solutions zs
     """
-    # Reducing the lattice basis L to a smaller basis B
-    B = fplll_to_matrix(LLL.reduction(matrix_to_fplll(L)))
-
-    Bys = Matrix(B @ ys)
-
-    # TODO: There might be a better solution to find the individual ks
-    # NB: B * (ys + zs) = m * ks for some ks
-    ks = Matrix([round(x) for x in Bys / m])
-
-    # We know solve the system of linear equations B zs = m * ks - B ys for zs
-    Bzs = Matrix(m * ks - Bys)
-    zs = B.solve(Bzs)
-    assert B @ zs == Bzs
-    return Matrix(zs)
-
-def reconstruct_lower_bits_flint(L, m, ys):
-    from flint import fmpz_mat
     L = fmpz_mat([[*row] for row in L])
     ys = fmpz_mat([[y] for y in ys])
     B = L.lll()
@@ -52,7 +21,7 @@ def reconstruct_lower_bits_flint(L, m, ys):
 
     # TODO: There might be a better solution to find the individual ks
     # NB: B * (ys + zs) = m * ks for some ks
-    ks = fmpz_mat([[round(int(x)/m)] for x in Bys])
+    ks = fmpz_mat([[round(int(x) / m)] for x in Bys])
 
     # We know solve the system of linear equations B zs = m * ks - B ys for zs
     Bzs = m * ks - Bys
