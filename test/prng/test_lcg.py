22c1a6b1f891c3f1c1c3be11d51d1f800f33e19c
malet <malet@invalid>
2018-12-15 16:38:27 +0100

    trying to find sensible tests

--- test/prng/test_lcg.py
+++ test/prng/test_lcg.py
@@ -1,39 +1,71 @@
+from hypothesis import assume, example, given, settings
+from hypothesis.strategies import floats, integers
+
 from cat.prng.lcg import *
-from hypothesis import given, assume, example
-from hypothesis.strategies import integers, floats
+from gmpy2 import mpz, next_prime
 
 
 def test_reconstruct_lower_bits_sanity():
-    m = 4294967291
-    a = 598176085
-    s = 252291025
+    m = 4_294_967_291
+    a = 598_176_085
+    s = 252_291_025
     L = [[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2, 0, -1, 0], [a ** 3, 0, 0, -1]]
-    xs = [1477951715, 3597964208, 2802631510, 3169049466]
+    xs = [1_477_951_715, 3_597_964_208, 2_802_631_510, 3_169_049_466]
     ys = get_upper_bits(xs, 16)
     zs = reconstruct_lower_bits(L, m, ys)
     assert zs == [49379, 37808, 50006, 56186]
 
 
-@given(integers(3), integers(2), integers(2), floats(1 / 2, 1))
-def test_reconstruct_lower_bits(m, a, s, bits_proportion):
-    assume(a < m and s < m)
+def test_reconstruct_lower_bits_prime():
+    m = int(next_prime(2 ** 32))
+    a = int(next_prime(2 ** 30))
+    s = 252_291_025
+    size = 20
+    L = [[m] + (size - 1) * [0]] + [
+        ([pow(a, i, m)] + [0] * (i - 1) + [-1] + [0] * (size - i - 1))
+        for i in range(1, size)
+    ]
+    assert len(L) == size and all([len(l) == size for l in L])
+
+    xs = [(a ** i * s) % m for i in range(1, size + 1)]
+    assert len(xs) == size
+
+    ys = get_upper_bits(xs, 16)
+    zs = reconstruct_lower_bits(L, m, ys)
+
+    assert xs[0] == (ys[0] + zs[0]) % m
+
+
+@settings(max_iterations=10000)
+@given(integers(2), integers(2), floats(1 / 2, 1))
+def test_reconstruct_lower_bits(m, s, bits_proportion):
+    assume(s < m)
+    m = int(next_prime(m))
+    a = 2
+    size = 25
     nbits = int(m.bit_length() * bits_proportion)
-    L = [[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2 % m, 0, -1, 0], [a ** 3 % m, 0, 0, -1]]
-    xs = [(a ** (i + 1) * s) % m for i in range(4)]
+
+    L = [[m] + (size - 1) * [0]] + [
+        ([pow(a, i, m)] + [0] * (i - 1) + [-1] + [0] * (size - i - 1))
+        for i in range(1, size)
+    ]
+    assert len(L) == size and all([len(l) == size for l in L])
+
+    xs = [(a ** i * s) % m for i in range(1, size + 1)]
+    assert len(xs) == size
+
     ys = get_upper_bits(xs, nbits)
+    # ys can't be 0
+    assume(all(y != 0 for y in ys))
     zs = [(x - y) % m for x, y in zip(xs, ys)]
     recovered_zs = reconstruct_lower_bits(L, m, ys)
-    # assert zs.table() == expected_zs
-    s_prime = int(recovered_zs[0] + ys[0])
-    if s_prime == s:
-        # Test pass, the seed is correct
-        return
-    print("s' - s = ", s_prime - s)
-    print(f"m={m}, a={a}, s={s}")
-    xs_prime = [(a ** i * s_prime) % m for i in range(4)]
-    zs_prime = [(x - y) % m for x, y in zip(xs_prime, get_upper_bits(xs_prime, nbits))]
-    print("Actual Expect:", zs)
-    print("Actual    Got:", recovered_zs)
-    assert zs == recovered_zs
-    print("Expect:", zs_prime, "Got:", recovered_zs)
-    assert zs_prime == recovered_zs
+
+    if xs[0] != (ys[0] + recovered_zs[0]) % m:
+        print()
+        print(f"m = {m}, a = {a}, s={a}")
+        print(
+            f"x_0 = {xs[0]}, y_0 = {ys[0]} + rz_0={recovered_zs[0]} = {ys[0] + recovered_zs[0]},"
+        )
+        print(zs)
+        print(recovered_zs)
+    assert xs[0] == (ys[0] + recovered_zs[0]) % m
