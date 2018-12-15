00c7a70697cb4fbb8325484b781576a570f11000
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-12-15 22:11:43 +0100

    Add simple logging configuration
    Follow common advice that a library should silence its logs. Provide
    an easy way to unsilence the logs.

--- cat/__init__.py
+++ cat/__init__.py
@@ -1,4 +1,8 @@
+import logging
+
 from .utils.checkresult import CheckResult, Severity
 from .oracle import Oracle
 
 __version__ = "0.0.1"
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
