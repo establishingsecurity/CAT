5a5a5cb594fcdc591a89a6f1d614663b1c03dda6
ambiso <ambiso@invalid>
2018-12-15 14:52:22 +0100

    Remove sympy implementation

--- test/prng/test_lcg.py
+++ test/prng/test_lcg.py
@@ -19,39 +19,26 @@ def test_reconstruct_lower_bits_sanity():
     assert zs == expected_zs
     assert xs == (ys + zs)
 
-def test_reconstruct_lower_bits_sanity_flint():
+
+def test_reconstruct_lower_bits_sanity():
     m = 4294967291
     a = 598176085
     s = 252291025
     L = [[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2, 0, -1, 0], [a ** 3, 0, 0, -1]]
     xs = [1477951715, 3597964208, 2802631510, 3169049466]
     ys = get_upper_bits(xs, 16)
-    zs = reconstruct_lower_bits_flint(L, m, ys)
+    zs = reconstruct_lower_bits(L, m, ys)
     assert zs.table() == [[49379], [37808], [50006], [56186]]
 
-@given(integers(2**8), integers(2), integers(), floats(1 / 2, 1))
-def test_reconstruct_lower_bits_flint(m, a, s, bits_proportion):
+
+@given(integers(2 ** 8), integers(2), integers(), floats(1 / 2, 1))
+def test_reconstruct_lower_bits(m, a, s, bits_proportion):
     assume(a < m and s < m)
     assume(m != 406)
     nbits = int(m.bit_length() * bits_proportion)
     L = [[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2 % m, 0, -1, 0], [a ** 3 % m, 0, 0, -1]]
     xs = [(a ** (i + 1) * s) % m for i in range(4)]
     ys = get_upper_bits(xs, nbits)
-    expected_zs = [[x - y] for x,y in zip(xs, ys)]
-    zs = reconstruct_lower_bits_flint(L, m, ys)
-    assert zs.table() == expected_zs
-
-@example(m=406, a=2, s=1, bits_proportion=0.5)
-@given(integers(0), integers(2), integers(), floats(1 / 2, 1))
-def test_reconstruct_lower_bits(m, a, s, bits_proportion):
-    assume(a < m and s < m)
-    nbits = int(m.bit_length() * bits_proportion)
-    assume(nbits > 2)
-    L = Matrix([[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2, 0, -1, 0], [a ** 3, 0, 0, -1]])
-    xs = Matrix([(a ** (i + 1) * s) % m for i in range(4)])
-    ys = Matrix(get_upper_bits(xs, nbits))
+    expected_zs = [[x - y] for x, y in zip(xs, ys)]
     zs = reconstruct_lower_bits(L, m, ys)
-    expected_zs = Matrix(xs - ys)
-    print(zs, expected_zs)
-    assert xs == (ys + zs)
-    assert zs == expected_zs
+    assert zs.table() == expected_zs
