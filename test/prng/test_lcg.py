828527110f1facedbd607797633fcdfdedc1384d
malet <malet@invalid>
2018-12-15 18:16:00 +0100

    Fix all tests

--- test/prng/test_lcg.py
+++ test/prng/test_lcg.py
@@ -4,10 +4,12 @@ from hypothesis.strategies import floats, integers
 from cat.prng.lcg import *
 from gmpy2 import mpz, next_prime
 
-def get_upper_bits(v, n=None):
+
+def blank_lower_bits(v, n=None):
     if n is None:
         n = max([e.bit_length() // 2 for e in v])
-    return [x - (x % 2 ** (x.bit_length()-n)) for x in v]
+    return [x - (x % 2 ** n) for x in v]
+
 
 def test_reconstruct_lower_bits_sanity():
     m = 4_294_967_291
@@ -15,40 +17,45 @@ def test_reconstruct_lower_bits_sanity():
     s = 252_291_025
     L = [[m, 0, 0, 0], [a, -1, 0, 0], [a ** 2, 0, -1, 0], [a ** 3, 0, 0, -1]]
     xs = [1_477_951_715, 3_597_964_208, 2_802_631_510, 3_169_049_466]
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, 16)
     zs = reconstruct_lower_bits(L, m, ys)
     assert zs == [49379, 37808, 50006, 56186]
+
 
 def test_reconstruct_lower_bits_few_outputs():
     m = int(next_prime(2 ** 64))
     a = int(next_prime(2 ** 30))
     s = 252_291_025
+    shift = 64 - 32
     size = 2
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     # Only two states should not be able to predictable (even information theoretically?)
     assert xs[0] != (ys[0] + zs[0]) % m
     assert all((x != y + z % m) for x, y, z in zip(xs, ys, zs))
 
+
 def test_reconstruct_lower_bits_few_bits():
     m = int(next_prime(2 ** 512))
     a = int(next_prime(2 ** 128))
     s = 252_291_025
-    size = 5
+    shift = 512 - 32
+    size = 15
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     # Only a few bits should not be able to predictable (even information theoretically?)
-    assert xs[0] == (ys[0] + zs[0]) % m
+    assert xs[0] != (ys[0] + zs[0]) % m
+
 
 @settings(max_iterations=10000)
 @example(s=252_291_025)
@@ -56,12 +63,13 @@ def test_reconstruct_lower_bits_few_bits():
 def test_reconstruct_lower_bits_prime_30(s):
     m = int(next_prime(2 ** 32))
     a = int(next_prime(2 ** 30))
+    shift = 32 - 16
     size = 20
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
@@ -74,13 +82,13 @@ def test_reconstruct_lower_bits_glibc_params(s):
     a = 1_103_515_245
     # b = 12345
     m = 2 ** 32
-    shift = 16
+    shift = 32 - 16
     size = 5
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, shift)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
@@ -90,16 +98,16 @@ def test_reconstruct_lower_bits_glibc_params(s):
 @example(s=252_291_025)
 @given(integers(2))
 def test_reconstruct_lower_bits_java_params(s):
+    m = 2 ** 48
     a = 0x5DEECE66D
     # b = 0xbl
-    m = 2 ** 48
-    shift = 16
+    shift = 48 - 16
     size = 5
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, shift)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
@@ -112,12 +120,13 @@ def test_reconstruct_lower_bits_java_params(s):
 def test_reconstruct_lower_bits_prime_60(s):
     m = int(next_prime(2 ** 64))
     a = int(next_prime(2 ** 32))
+    shift = 64 - 16
     size = 10
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
@@ -130,12 +139,13 @@ def test_reconstruct_lower_bits_prime_60(s):
 def test_reconstruct_lower_bits_prime_128(s):
     m = int(next_prime(2 ** 128))
     a = int(next_prime(2 ** 64))
+    shift = 128 - 64
     size = 10
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 16)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
@@ -148,12 +158,13 @@ def test_reconstruct_lower_bits_prime_128(s):
 def test_reconstruct_lower_bits_prime_512(s):
     m = int(next_prime(2 ** 512))
     a = int(next_prime(2 ** 256))
+    shift = 512 - 256
     size = 10
 
     xs = [(a ** i * s) % m for i in range(1, size + 1)]
     L = construct_lattice(m, a, size)
 
-    ys = get_upper_bits(xs, 128)
+    ys = blank_lower_bits(xs, shift)
     zs = reconstruct_lower_bits(L, m, ys)
 
     assert xs[0] == (ys[0] + zs[0]) % m
