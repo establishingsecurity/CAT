72c77b1b0e6c132c8d8564fe9cb61289e74dc12f
Aleksandr Lisianoi <alisianoi@protonmail.com>
2019-01-24 11:09:37 +0100

    Move factorization to separate package

--- test/factorize/test_fermat.py
+++ test/factorize/test_fermat.py
@@ -1,21 +1,19 @@
 import pytest
 
-from cat.rsa.fermat import (
-    factor
-)
+from cat.factorize.fermat import factor
 
 
 @pytest.mark.parametrize(
     "data",
     [
-        [9 , [3, 3]],
+        [9, [3, 3]],
         [15, [3, 5]],
         [21, [3, 7]],
         [25, [5, 5]],
         [27, [3, 9]],
         [33, [3, 11]],
         [35, [5, 7]],
-    ]
+    ],
 )
 def test_factor_small_composite(data):
 
@@ -24,9 +22,7 @@ def test_factor_small_composite(data):
     assert factor(product) in factors
 
 
-@pytest.mark.parametrize(
-    "prime", [3, 5, 7, 11, 13, 17, 19, 23]
-)
+@pytest.mark.parametrize("prime", [3, 5, 7, 11, 13, 17, 19, 23])
 def test_factor_small_prime(prime):
 
     assert factor(prime) == 1
