3db47a939792436aa6a72fd8e9615cb44e7791a5
Michael Strasser <mst1409@gmx.at>
2019-01-24 14:22:39 +0100

    There's `pathlib` in Python2.7, but it's called `pathlib2`

--- cat/__init__.py
+++ cat/__init__.py
@@ -3,6 +3,12 @@ import logging
 from .utils.checkresult import CheckResult, Severity
 from .oracle import Oracle
 
+# use pathlib.Path in python2.7
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
 __version__ = "0.0.1"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
