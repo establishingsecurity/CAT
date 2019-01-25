f80a8d69fc5acb3c0df6dcf887e721e5270bdabf
malet <malet@invalid>
2019-01-25 23:55:56 +0100

    About 2x performance improvment :)

--- cat/prng/lcg.py
+++ cat/prng/lcg.py
@@ -30,7 +30,10 @@ def construct_lattice(m, a, size):
         for i in range(1, size)
     ]
     assert len(L) == size and all([len(l) == size for l in L])
-    return L
+    L = fmpz_mat([row for row in L])
+    # Reducing the lattice basis L to a smaller basis B
+    B = L.lll()
+    return B
 
 
 def _round_fmpq(x):
@@ -40,20 +43,18 @@ def _round_fmpq(x):
     return p // q + (1 if r >= q // 2 and q > 1 else 0)
 
 
-def reconstruct_lehmer_lower(L, m, ys):
+def reconstruct_lehmer_lower(B, m, ys):
     """
     Reconstructs the lower bits :math:`zs` of a system of linear congurential equations in lattice form with
-    :math:`L \\cdot xs = 0 \\mod m` for solution :math:`xs`, where :math:`xs = ys + zs`.
+    :math:`B \\cdot xs = 0 \\mod m` for solution :math:`xs`, where :math:`xs = ys + zs`.
     It works by computing a smaller basis :math:`B` from the basis :math:`L` and solving equations in the smaller basis.
 
-    :param L: The system of linear equation in matrix form (evaluating to 0)
+    :param B: The system of linear equation in matrix form (evaluating to 0)
     :param m: The modulus used for all the equations
     :param ys: Partial solutions for the variables (xs = ys + zs)
     :return: The remaining operands of the solutions zs
     """
-    L = fmpz_mat([row for row in L])
     ys = fmpz_mat([[y] for y in ys])
-    B = L.lll()  # Reducing the lattice basis L to a smaller basis B
 
     Bys = B * ys
 
