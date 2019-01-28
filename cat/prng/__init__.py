90262c98db1ee0c662261b9a2421c69378f2ea31
ambiso <ambiso@invalid>
2019-01-28 16:43:31 +0100

    Add glibc documentation

--- cat/prng/__init__.py
+++ cat/prng/__init__.py
@@ -1,2 +1,5 @@
+"""
+Parameters for glibc's rand_r or when invoking initstate with an 8 byte state.
+"""
 glibc_lcg_params = {"m": 2 ** 32, "a": 1103515245, "b": 12345, "shift": 0}
-java_lcg_params = {"m": 2 ** 48, "a": 0x5DEECE66D, "b": 0xB, "shift": 48-16}
+java_lcg_params = {"m": 2 ** 48, "a": 0x5DEECE66D, "b": 0xB, "shift": 48 - 16}
