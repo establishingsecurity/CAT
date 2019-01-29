cc1aba5d88813837461e8f2675c48b32d1d6411b
Michael Strasser <mst1409@gmx.at>
2019-01-29 15:31:11 +0100

    Modify RSA checks to: - use Result class instead of CheckResult - no longer depend on Cryptodome

--- cat/rsa/checks.py
+++ cat/rsa/checks.py
@@ -1,24 +1,91 @@
-from Cryptodome.PublicKey import RSA
-
 import gmpy2
+from cat.utils.result import Result
+from cat.utils.result import ResultEntry as RE
+from cat.utils.result import Severity
 
-from . import RSAKey
-from .. import CheckResult, Severity
+
+class RSAResult(Result):
+    """
+    Results for checks on RSA components.
+    """
+
+    OK = RE(Severity.OK, """The RSA component is as one would expect.""")
+    SMALL_MODULUS = RE(Severity.SUSPICIOUS, """Modulus is smaller than 1023.""")
+    EVEN_MODULUS = RE(Severity.CRITICAL, """Modulus is even and therefore composite.""")
 
 
-def check_public(pk):
-    # type: RSAKey
+class RSAPublicKeyResult(RSAResult):
+    """
+    Results for checks on RSA public keys. The value of the members denote their severity.
+    """
+
+    NON_DEFAULT = RE(
+        Severity.OK,
+        "The public key is a deliberate deviation from the well-known, common default.",
+    )
+    LARGER_THAN_DEFAULT = RE(
+        Severity.OK, "Larger than default: will slow down RSA performance."
+    )
+    SMALLER_THAN_DEFAULT = RE(
+        Severity.OK,
+        "Smaller than default: (unlikely) could be weak if padding is weak.",
+    )
+    BROADCAST_ATTACK = RE(
+        Severity.OK,
+        (
+            "If the same message is encrypted roughly b times for different "
+            "values of N, where b is the Bound given to :func:`check_public_rsa_exponent`, "
+            "then the message could be recovered. "
+            'See "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, section 4.2.'
+        ),
+    )
+    PARTIAL_KEY_EXPOSURE = RE(
+        Severity.OK,
+        (
+            "If you could recover some bits of the private key, then maybe you could recover"
+            "the whole private key. "
+            'See "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, section 4.5.'
+        ),
+    )
+    NON_PRIME = RE(Severity.SUSPICIOUS, "The public key's exponent is non-prime.")
+
+
+class RSAPrivateKeyResult(RSAResult):
+    """
+    Results for checks on RSA private keys. The value of the members denote their severity.
+    """
+
+    SMALL_PRIVATE_EXPONENT = RE(
+        Severity.SUSPICIOUS,
+        "Very small private exponent: could be vulnerable to Wiener 1990",
+    )
+
+
+def check_public_key(N, e):
+    # type: (int, int) -> Tuple[RSAResult, RSAResult, RSAPublicKeyResult]
     """
     Checks the RSA public key for anything "easy and suspicious"
 
-    :param pk: the RSA public key
+    :param e: The RSA public exponent.
+    :param N: The RSA modulus.
     :return: a list of check results
     """
-    return [check_composite(pk.n), check_modulus_size(pk.n), check_prime(pk.e)]
+    results = [check_composite(N), check_modulus_size(N), check_prime(e)]
+    results.extend(check_public_rsa_exponent(N, e))
+
+    return results
 
 
-def check_public_rsa_exponent(pk):
-    # type: RSAKey
+def check_modulus(n):
+    # type: (int) -> RSAResult
+    """
+    Checks the RSA modulus for anything "easy and suspicious".
+    """
+    return [check_composite(n), check_modulus_size(n)]
+
+
+def check_public_rsa_exponent(N, e, BOUND=512):
+    # type: (RSAKey, int) -> List[RSAPublicKeyResult]
     """
     Checks if the public RSA exponent is non-standard
 
@@ -33,91 +100,73 @@ def check_public_rsa_exponent(pk):
     """
 
     results = []
+    results.extend(_check_public_exponent(e, BOUND))
+    results.extend(_check_pub_exp_modulus(e, N))
+    return results
 
-    if pk.e != 65537:
-        results.append(
-            CheckResult(
-                Severity.OK,
-                "This is a deliberate deviation from the well-known, common default.",
-            )
-        )
 
-    if pk.e > 65537:
-        results.append(
-            CheckResult(
-                Severity.OK, "Larger than default: will slow down RSA performance."
-            )
-        )
+def _check_public_exponent(e, BOUND=512):
+    results = []
+
+    if e != 65537:
+        results.append(RSAPublicKeyResult.NON_DEFAULT)
+
+    if e > 65537:
+        results.append(RSAPublicKeyResult.LARGER_THAN_DEFAULT)
 
     # Based on https://crypto.stackexchange.com/a/3113/23435
-    if pk.e < 65537:
-        results.append(
-            CheckResult(
-                Severity.OK,
-                "Smaller than default: (unlikely) could be weak if padding is weak.",
-            )
-        )
+    if e < 65537:
+        results.append(RSAPublicKeyResult.SMALLER_THAN_DEFAULT)
 
     # From "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, where
     # section 4.2 mentions "Hastard's Broadcast attack"
     # The if block below uses an arbitrary choice of a small constant
-    if pk.e < 512:
-        result.append(
-            CheckResult(
-                Severity.OK,
-                """
-                If the same message is encrypted roughly {} times for different
-                values of N, then the message could be recovered.
-                """.format(
-                    e
-                ),
-            )
-        )
-
-    # From "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, where
-    # section 4.5 mentions "Partial Key Exposure Attack"
-    # This will be a very noisy check though, very often true.
-    if pk.e < gmpy2.sqrt(n):
-        result.append(
-            CheckResult(
-                Severity.OK,
-                """
-                If you could recover some bits of the private key, then maybe
-                you could recover the whole private key.
-                """,
-            )
-        )
+    if e < BOUND:
+        results.append(RSAPublicKeyResult.BROADCAST_ATTACK)
 
     return results
 
 
-def check_private_rsa_exponent(pk):
-    # type: RSAKey
+def _check_pub_exp_modulus(e, n):
+    results = []
+
+    # From "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, where
+    # section 4.5 mentions "Partial Key Exposure Attack"
+    # This will be a very noisy check though, very often true.
+    if e < gmpy2.sqrt(n):
+        results.append(RSAPublicKeyResult.PARTIAL_KEY_EXPOSURE)
+
+    return results
+
+
+def check_private_key(N, d):
+    return [check_composite(N), check_modulus_size(N), check_private_rsa_exponent(N, d)]
+
+
+def check_private_rsa_exponent(N, d):
+    # type: (int, int) -> RSAPrivateKeyResult
 
     # From "Twenty Years of Attacks on the RSA Cryptosystem" by Dan Boneh, where
     # section 3 mentions "Cryptanalysis of short RSA secret exponents" by Wiener
-    if pk.d < 0.3 * pow(pk.n, 1 / 4):
-        return CheckResult(
-            Severity.SUSPICIOUS,
-            "Very small private exponent: could be vulnerable to Wiener 1990",
-        )
+    if d < 0.3 * pow(N, 1 / 4):
+        return RSAPrivateKeyResult.SMALL_PRIVATE_EXPONENT
+    return RSAPrivateKeyResult.OK
 
 
 def check_prime(p):
-    # type: int
+    # type: (int) -> RSAPublicKeyResult
     """
     Checks if given number is a prime
 
     :p the potential prime
     """
     if not gmpy2.is_prime(p):
-        return CheckResult(Severity.SUSPICIOUS, "Not a prime")
-
-    return CheckResult()
+        return RSAPublicKeyResult.NON_PRIME
+    return RSAPublicKeyResult.OK
 
 
 def check_modulus_size(n):
-    # type: int
+    # type: (int) -> RSAResult
     """
     Checks if the modulus has good RSA sizes
 
@@ -126,19 +175,17 @@ def check_modulus_size(n):
     # TODO: Research exact size
 
     if gmpy2.log2(n) < 1023:
-        return CheckResult(Severity.SUSPICIOUS, "Modulus too small")
-
-    return CheckResult()
+        return RSAResult.SMALL_MODULUS
+    return RSAResult.OK
 
 
 def check_composite(n):
-    # type: int
+    # type: (int) -> RSAResult
     """
     Checks if a number is probably composite in a RSA sense
 
     :n The composite number
     """
     if n % 2 == 0:
-        return CheckResult(Severity.CRITICAL, "Modulus is even")
-
-    return CheckResult()
+        return RSAResult.EVEN_MODULUS
+    return RSAResult.OK
