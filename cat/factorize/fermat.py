6a67b69cbfd4e22699a48d81851912fa887e0f2a
malet <malet@invalid>
2019-02-01 16:07:32 +0100

    adding a bit of caching

--- cat/factorize/fermat.py
+++ cat/factorize/fermat.py
@@ -2,10 +2,12 @@ import logging
 
 import gmpy2
 from cat.log.log import LIB_ROOT_LOGGER_NAME as LOGGER
+from cat.utils.snapshots import long_running
 
 PRECISION_BITS = 2048
 
 
+@long_running
 def factor(N):
     # type: mpz
     """
