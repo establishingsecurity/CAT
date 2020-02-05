4e27dd45476ba334e7dae0dc0ac86b8d4fe7ac1d
malet <malet@invalid>
2020-02-05 15:39:38 +0100

    Using proper pytest marks

--- conftest.py
+++ conftest.py
@@ -5,9 +5,6 @@ from cat.log import enable_logging
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--slow", action="store_true", default=False, help="run slow tests"
-    )
-    parser.addoption(
         "--enable-logs",
         "--enable-logging",
         action="store_true",
@@ -17,13 +14,5 @@ def pytest_addoption(parser):
 
 
 def pytest_collection_modifyitems(config, items):
-    if not config.getoption("--slow"):
-        # --slow given in cli: do not skip slow tests
-
-        skip_slow = pytest.mark.skip(reason="need --slow option to run")
-        for item in items:
-            if "slow" in item.keywords:
-                item.add_marker(skip_slow)
-
     if config.getoption("--enable-logs"):
         enable_logging()
