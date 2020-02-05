d766a849b8d855909134cd5b7f823e253ea4cda0
malet <malet@invalid>
2020-02-05 15:48:18 +0100

    Revert "Using proper pytest marks"
    This reverts commit 4e27dd45476ba334e7dae0dc0ac86b8d4fe7ac1d.

--- conftest.py
+++ conftest.py
@@ -5,6 +5,9 @@ from cat.log import enable_logging
 
 def pytest_addoption(parser):
     parser.addoption(
+        "--slow", action="store_true", default=False, help="run slow tests"
+    )
+    parser.addoption(
         "--enable-logs",
         "--enable-logging",
         action="store_true",
@@ -14,5 +17,13 @@ def pytest_addoption(parser):
 
 
 def pytest_collection_modifyitems(config, items):
+    if not config.getoption("--slow"):
+        # --slow given in cli: do not skip slow tests
+
+        skip_slow = pytest.mark.skip(reason="need --slow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
+
     if config.getoption("--enable-logs"):
         enable_logging()
