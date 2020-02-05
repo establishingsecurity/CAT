8c19e2a90f6e23a8936eeadee6f8324b11754d6d
malet <malet@invalid>
2020-02-05 17:42:21 +0100

    Mypy vomits on this

--- cat/__init__.py
+++ cat/__init__.py
@@ -1,14 +1,15 @@
-import sys
 import logging
+import sys
 
+from .oracle import Oracle
+from .utils.result import Result, Severity
+
+# mypy: ignore-errors
 if sys.version_info.major >= 3:
     from pathlib import Path
 else:
     from pathlib2 import Path
 
-
-from .oracle import Oracle
-from .utils.result import Result, Severity
 
 __version__ = "0.0.1"
 
